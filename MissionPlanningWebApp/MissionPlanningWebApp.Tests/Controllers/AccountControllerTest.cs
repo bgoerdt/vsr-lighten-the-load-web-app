2dd4700d7b9fecfc4c7a2d3709c55f4c3199ecef
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-02-20 14:35:18 -0600

    Copied project over. This repo is much cleaner due to the .gitignore file added upon creation.

---
+++ MissionPlanningWebApp/MissionPlanningWebApp.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MissionPlanningWebApp.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void Login()
+        {
+        }
+    }
+}
