b6a2fd8c66a1da571076eedfa89588feea62f780
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-02-28 13:17:35 -0600

    Added MissionRule Model

---
+++ MissionPlanningWebApp/MissionPlanningWebApp/Models/MissionRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MissionPlanningWebApp.Models
+{
+	public class MissionRule
+	{
+		//MParaIndex, RuleCond, RuleData, EquipIndex, ConstrCond, ConstrRHS
+
+		public int ID { get; set; }
+		public int ParamIndex { get; set; }
+		public char RuleCond { get; set; }
+		public int RuleData { get; set; }
+		public int EquipIndex { get; set; }
+		public int ConstrCond { get; set; }
+		public int ConstrRHS { get; set; }
+	}
+
+	public class MissionRuleDbContext : DbContext
+	{
+		public DbSet<MissionRule> MissionRules { get; set; }
+	}
+}
