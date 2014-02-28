1ef729f521695c53e7949867b8c08f45977e9287
Melanie Kneisel <melanie-kneisel@uiowa.edu>
2014-02-28 12:40:45 -0600

    adding DistributionRules model

---
+++ MissionPlanningWebApp/MissionPlanningWebApp/Models/DistributionRules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MissionPlanningWebApp.Models
+{
+    public class DistributionRules
+    {
+        public int ID { get; set; }
+        public int ChrIndex { get; set; }
+        public char ChrCond { get; set; }
+        public int ChrData { get; set; }
+        public int EquipIndex { get; set; }
+        public char ConstrCond { get; set; }
+        public int ConstrRHS { get; set; }
+    }
+
+    public class DistributionRulesDBContext : DbContext
+    {
+        public DbSet<DistributionRules> DistributionRules { get; set; }
+    }
+}
