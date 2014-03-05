8f42e13bb44ef73c33fe0e59a9c25045bd7d8a8a
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-03-05 12:28:03 -0600

    Started writing tests for the MissionRule Controller

---
+++ MissionPlanningWebApp/MissionPlanningWebApp.Tests/Controllers/MissionRuleControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MissionPlanningWebApp.Controllers;
+
+namespace MissionPlanningWebApp.Tests.Controllers
+{
+	[TestClass]
+	public class MissionRuleControllerTest
+	{
+		[TestMethod]
+		public void Index()
+		{
+			MissionRuleController missRuleController = new MissionRuleController();
+			ViewResult viewResult = missRuleController.Index() as ViewResult;
+
+			Assert.AreEqual(viewResult.ViewName, "MissionRule");
+		}
+
+		[TestMethod]
+		public void Details()
+		{
+			MissionRuleController missRuleController = new MissionRuleController();
+			ViewResult viewResult = missRuleController.Details(5) as ViewResult;
+
+			Assert.AreEqual(viewResult.ViewName, "MissionRule/Details/5");
+		}
+
+		[TestMethod]
+		public void Create()
+		{
+			MissionRuleController missRuleController = new MissionRuleController();
+			ViewResult viewResult = missRuleController.Create() as ViewResult;
+
+			Assert.AreEqual(viewResult.ViewName, "MissionRule/Create");
+		}
+
+		[TestMethod]
+		public void Edit()
+		{
+			MissionRuleController missRuleController = new MissionRuleController();
+			ViewResult viewResult = missRuleController.Edit(5) as ViewResult;
+
+			Assert.AreEqual(viewResult.ViewName, "MissionRule/Edit/5");
+		}
+
+		[TestMethod]
+		public void Delete()
+		{
+			MissionRuleController missRuleController = new MissionRuleController();
+			ViewResult viewResult = missRuleController.Delete(5) as ViewResult;
+
+			Assert.AreEqual(viewResult.ViewName, "MissionRule/Delete/5");
+		}
+	}
+}
