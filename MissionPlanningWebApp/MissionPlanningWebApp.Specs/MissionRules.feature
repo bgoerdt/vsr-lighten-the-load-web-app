e52bb483ed63b81c777d38f9db9206be9ff010f0
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-03-06 15:37:30 -0600

    Started writing basic SpecFlow tests. Need to do some more research.

---
+++ MissionPlanningWebApp/MissionPlanningWebApp.Specs/MissionRules.feature
@@ -0,0 +1,11 @@
+﻿Feature: Mission rules
+
+Scenario: View all mission rules
+	Given you are on the home page
+	And these mission rules exist
+	  | ID | ParamIndex | RuleCond | RuleData | EquipIndex | ConstrCond | ConstrRHS |
+	  | 0  | 0          | >        | 10       | 0          | =          | 5         |
+	  | 1  | 0          | >        | 20       | 0          | =          | 10        |
+	  | 2  | 0          | >        | 30       | 0          | =          | 15        |
+	When you click "Mission Rules"
+	Then the mission rules should be displayed
--- MissionPlanningWebApp/MissionPlanningWebApp/Models/MissionRule.cs
+++ MissionPlanningWebApp/MissionPlanningWebApp/Models/MissionRule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
@@ -8,14 +9,37 @@ namespace MissionPlanningWebApp.Models
 {
 	public class MissionRule
 	{
-		//MParaIndex, RuleCond, RuleData, EquipIndex, ConstrCond, ConstrRHS
+		// constructor
+		public MissionRule(int id, int pInd, string rCond, int rDat, int eInd, string constrCond, int constrRHS)
+		{
+			ID = id;
+			ParamIndex = pInd;
+			RuleCond = rCond;
+			RuleData = rDat;
+			EquipIndex = eInd;
+			ConstrCond = constrCond;
+			ConstrRHS = constrRHS;
+		}
 
+		//MParaIndex, RuleCond, RuleData, EquipIndex, ConstrCond, ConstrRHS
 		public int ID { get; set; }
+
+		[Required]
 		public int ParamIndex { get; set; }
+
+		[Required]
 		public string RuleCond { get; set; }
+
+		[Required]
 		public int RuleData { get; set; }
+
+		[Required]
 		public int EquipIndex { get; set; }
+
+		[Required]
 		public string ConstrCond { get; set; }
+
+		[Required]
 		public int ConstrRHS { get; set; }
 	}
 
