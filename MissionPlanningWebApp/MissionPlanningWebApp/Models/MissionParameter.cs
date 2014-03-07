6d542cc8c7d1fb83dde1ee511808b2e0d881bd89
Gunnar Mills <gunnar-mills@uiowa.edu>
2014-03-07 11:42:19 -0600

    fixed database problem on my branch

--- MissionPlanningWebApp/MissionPlanningWebApp/Models/MissionParameter.cs
+++ MissionPlanningWebApp/MissionPlanningWebApp/Models/MissionParameter.cs
@@ -10,6 +10,7 @@ namespace MissionPlanningWebApp.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public int Value { get; set; }
     }
 
     public class MissionParameterDbContext : DbContext
