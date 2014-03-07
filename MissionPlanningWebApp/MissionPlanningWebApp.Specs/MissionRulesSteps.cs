d96bb123dfc090109497164ef6081caad462b566
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-03-07 12:17:30 -0600

    Added some specflow tests. NOT WORKING

---
+++ MissionPlanningWebApp/MissionPlanningWebApp.Specs/MissionRulesSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MissionPlanningWebApp.Tests
+{
+    [Binding]
+    public class MissionRulesSteps
+    {
+        [Given(@"you are on the home page")]
+        public void GivenYouAreOnTheHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"these mission rules exist")]
+        public void GivenTheseMissionRulesExist(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"you click ""(.*)""")]
+        public void WhenYouClick(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the mission rules should be displayed")]
+        public void ThenTheMissionRulesShouldBeDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
