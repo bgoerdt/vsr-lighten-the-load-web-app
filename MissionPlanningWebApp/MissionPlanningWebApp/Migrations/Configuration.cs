1049559a103b2d339631c3f9c134a66c976494cd
Melanie Kneisel <melanie-kneisel@uiowa.edu>
2014-04-04 12:31:49 -0500

    created db seed

--- MissionPlanningWebApp/MissionPlanningWebApp/Migrations/Configuration.cs
+++ MissionPlanningWebApp/MissionPlanningWebApp/Migrations/Configuration.cs
@@ -2,6 +2,7 @@ namespace MissionPlanningWebApp.Migrations
 {
     using MissionPlanningWebApp.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,4 +15,29 @@ namespace MissionPlanningWebApp.Migrations
     //    }
 
     //}
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MissionPlanningWebApp.Models.LtLDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(LtLDbContext context)
+        {
+ 	        base.Seed(context);
+
+            var characteristics = new List<Characteristic>()
+            { 
+                //Toughness, Size/Strength, Experience, PFT Score
+                new Characteristic { Char = "Toughness" },
+                new Characteristic { Char = "Size/Strength"},
+                new Characteristic { Char = "Experience" },
+                new Characteristic { Char = "PFT Score" }
+            };
+
+            characteristics.ForEach(chr => context.Characteristics.AddOrUpdate(c => c.Char, chr));
+            context.SaveChanges();
+        }
+    }
 }
