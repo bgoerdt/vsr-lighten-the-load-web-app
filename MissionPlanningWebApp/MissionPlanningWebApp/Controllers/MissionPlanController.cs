3af400505b6e8c4d38ad9f363022f664c64e2825
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-04-15 13:04:58 -0500

    Added MissionPlan model and controller with structure but no functionality. Added MissionData directory with text files.

---
+++ MissionPlanningWebApp/MissionPlanningWebApp/Controllers/MissionPlanController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MissionPlanningWebApp.Models;
+
+namespace MissionPlanningWebApp.Controllers
+{
+    public class MissionPlanController : Controller
+    {
+        //
+        // GET: /MissionPlan/
+
+        public ActionResult Index(MissionPlan plan)
+        {
+            return View(plan);
+        }
+
+		[HttpPost]
+		public ActionResult Create()
+		{
+			MissionPlan plan = new MissionPlan();
+			plan.planMission();
+
+			return RedirectToAction("Index", plan);
+		}
+
+    }
+}
---
+++ MissionPlanningWebApp/MissionPlanningWebApp/Models/MissionPlan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MissionPlanningWebApp.Models;
+
+namespace MissionPlanningWebApp.Models
+{
+	public class MissionPlan
+	{
+		public Dictionary<Equipment, int> EquipmentList { get; set; }
+
+		public void planMission()
+		{
+			_exportData();
+
+			// call exe on server
+
+			_getMissionResults();
+		}
+
+		private void _exportData()
+		{
+			// export equipment to file
+			
+			// export parameters to file
+
+			// export mission rules to file
+		}
+
+		private void _getMissionResults()
+		{
+			// get results from MissionPlanning.txt and add to EquipmentList
+		}
+	}
+}
