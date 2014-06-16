a4a50e7a308c70b9ed73271e961adcd5573fd395
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-06-16 14:25:59 -0500

    Error handling

--- MissionPlanningWebApp/MissionPlanningWebApp/App_Start/FilterConfig.cs
+++ MissionPlanningWebApp/MissionPlanningWebApp/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace MissionPlanningWebApp
 	{
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
-			filters.Add(new HandleErrorAttribute());
+			//filters.Add(new HandleErrorAttribute());
 		}
 	}
 }
